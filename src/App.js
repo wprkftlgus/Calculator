3a609fffc2dbc7e4dc639cd3bc81a9e399512bb4
wprkftlgus <wprkftlgus@naver.com>
2025-10-19 18:29:49 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -61,36 +61,36 @@ function App() {
     <div className='title1'>Sihyeon's </div>
     <div className='title2'>Calculator</div>
     </div>
-    <div class='calcontain'>
+    <div className='calcontain'>
     <div className='holder-top'>
     <div className='network'></div>
     <div className='wifi'></div>
     <div className='battery'></div>
     </div>
     <div className='holder-inputAndResult'>
-    <input class='inputBar'readOnly value={calc} />
-    <input class='resultBar'readOnly value={result} />
+    <input className='inputBar'readOnly value={calc} />
+    <input className='resultBar'readOnly value={result} />
     </div>
     <div class='grid'>
-    <button class='button-top' onClick = {allClear}>AC</button>
-    <button class='button-top' onClick = {delCalc}>DEL</button>
-    <button class='button-top' value={'%'} onClick={getOper}>%</button>
-    <button class='operButton' value={'÷'} onClick={getOper}>/</button>
-    <button class='button' value={7} onClick={getNum}>7</button>
-    <button class='button' value={8} onClick={getNum}>8</button>
-    <button class='button' value={9} onClick={getNum}>9</button>
-    <button class='operButton' value={'×'} onClick={getOper}>×</button>
-    <button class='button' value={4} onClick={getNum}>4</button>
-    <button class='button' value={5} onClick={getNum}>5</button>
-    <button class='button' value={6} onClick={getNum}>6</button>
-    <button class='operButton' value={'-'} onClick={getOper}>-</button>
-    <button class='button' value={1} onClick={getNum}>1</button>
-    <button class='button' value={2} onClick={getNum}>2</button>
-    <button class='button' value={3} onClick={getNum}>3</button>
-    <button class='operButton' value={'+'} onClick={getOper}>+</button>
-    <button class='button' value={0} onClick={getNum}>0</button>
-    <button class='button' value={"."} onClick={getOper}>.</button>
-    <button class='operButton' value={'='} onClick={getResult}>=</button>
+    <button className='button-top' onClick = {allClear}>AC</button>
+    <button className='button-top' onClick = {delCalc}>DEL</button>
+    <button className='button-top' value={'%'} onClick={getOper}>%</button>
+    <button className='operButton' value={'÷'} onClick={getOper}>/</button>
+    <button className='button' value={7} onClick={getNum}>7</button>
+    <button className='button' value={8} onClick={getNum}>8</button>
+    <button className='button' value={9} onClick={getNum}>9</button>
+    <button className='operButton' value={'×'} onClick={getOper}>×</button>
+    <button className='button' value={4} onClick={getNum}>4</button>
+    <button className='button' value={5} onClick={getNum}>5</button>
+    <button className='button' value={6} onClick={getNum}>6</button>
+    <button className='operButton' value={'-'} onClick={getOper}>-</button>
+    <button className='button' value={1} onClick={getNum}>1</button>
+    <button className='button' value={2} onClick={getNum}>2</button>
+    <button className='button' value={3} onClick={getNum}>3</button>
+    <button className='operButton' value={'+'} onClick={getOper}>+</button>
+    <button className='button' value={0} onClick={getNum}>0</button>
+    <button className='button' value={"."} onClick={getOper}>.</button>
+    <button className='operButton' value={'='} onClick={getResult}>=</button>
     </div>
     <div className='holder-white'>
     <div className='white'></div>
